0a1a5f092b3f17798f38379440577a760bfc8ddb
Igor Solovev <solovyov.spb@gmail.com>
2025-12-08 02:18:41 +0300

    feat: fix base in config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/movie-hunter",
+  base: "/movie-hunter/",
   plugins: [
     tailwindcss(),
     react({
